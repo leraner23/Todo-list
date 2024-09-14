063a430e52bfdfdd5f65e916a984e754be337a61
Sujan Saru <160107123+leraner23@users.noreply.github.com>
2024-09-14 22:17:29 +0545

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 function Fun(e){
     e.preventDefault();
-  
     const name = document.getElementById("fname").value.trim();
- 
-    if (!name) {
+ if (!name) {
       alert("Please enter a name before submitting.");
       return; // Stop the function from executing further if the input is empty
     }
-
-    console.log("fname:", name);
+  console.log("fname:", name);
 
 // for storing multiple objects and value in local storage 
-
     const ItemN = {
       name: name,
        
@@ -25,25 +21,20 @@ function Fun(e){
 
     
 // to make <li> element in the Html code
- 
   const checklist = document.createElement("input"); 
   const newList = document.createElement("li");
   newList.textContent = "Name : "+ name;
-
   checklist.type = "checkbox";
-
   newList.prepend(checklist); 
 
   //for output 
  const outputList = document.querySelector(".output ul");
- 
   
  outputList.appendChild(newList);
 
   checklist.addEventListener('change', function() {
     newList.style.textDecorationLine = this.checked ? "line-through" : "none";
    
-
     if (this.checked) {
       outputList.appendChild(newList); // Move to bottom
     } else {
@@ -51,14 +42,6 @@ function Fun(e){
     }
   });
 
-
-
-
   // for inputing fresh input
-  document.getElementById("fname").value = "";
-  
-
-
-
-
+  document.getElementById("fname").value = "";  
 }   
